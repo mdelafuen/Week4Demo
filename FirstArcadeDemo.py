8abb648fa4ace483d00c50945dd741abcb5c1626
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-22 08:44:49 -0400

    finished house

--- FirstArcadeDemo.py
+++ FirstArcadeDemo.py
@@ -7,7 +7,10 @@ def main ():
     # now create objects
     main_house = arcade.create_rectangle(400, 200, 400, 400, arcade.color.ANTIQUE_BRASS)
     door = arcade.create_rectangle(400, 75, 100, 150, arcade.color.ARMY_GREEN)
-    window1 = arcade.create_ellipse(300, 300, 75, 75, arcade.color.BABY_BLUE)
+    window1 = arcade.create_ellipse(300, 300, 50, 50, arcade.color.BABY_BLUE)
+    window2 = arcade.create_ellipse(500, 300, 50, 50, arcade.color.BABY_BLUE)
+    roof_points = [(200, 400), (400, 600), (600, 400)]
+    roof = arcade.create_polygon(roof_points, arcade.color.DARK_GRAY)
 
     # now we will begin to draw
     arcade.start_render()
@@ -15,6 +18,8 @@ def main ():
     main_house.draw()
     door.draw()
     window1.draw()
+    window2.draw()
+    roof.draw()
 
     arcade.finish_render()
 
